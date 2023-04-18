48089d67a22a3e34bd0dc953613cd340a0c1de76
Gregory Ross <gregross.dev@gmail.com>
2023-04-18 15:44:05 -0400

    350. Intersection of Two Arrays II

--- 2-pointers/03-merge-2-arrays/Practice.java
+++ 2-pointers/03-merge-2-arrays/Practice.java
@@ -13,6 +13,33 @@ class Practice {
           nums1[tail--] = nums2[p2nums2--];
   }
 
+  // 350. Intersection of Two Arrays II
+  public int[] intersect(int[] nums1, int[] nums2) {
+      Arrays.sort(nums1);
+      Arrays.sort(nums2);
+
+      int p1nums1 = 0; 
+      int p2nums2 = 0;
+      List<Integer> cross = new ArrayList<>();
+      while(p1nums1 < nums1.length && p2nums2 < nums2.length) {
+          if(nums1[p1nums1] < nums2[p2nums2]) 
+              p1nums1++;
+          else if(nums1[p1nums1] > nums2[p2nums2])
+              p2nums2++;
+          else {
+              cross.add(nums1[p1nums1]);
+              p1nums1++;
+              p2nums2++;
+          }
+      }
+
+      int[] output = new int[cross.size()];
+      for(int k = 0; k < output.length; k++)
+        output[k] = cross.get(k);
+      
+      return output;
+  }
+
 }
 
 
