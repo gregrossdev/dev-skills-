55c7908d6e8e2e9094f8fdb0d14ab84b0284d8a1
Gregory Ross <gregross.dev@gmail.com>
2023-04-18 11:30:04 -0400

    26. Remove Duplicates from Sorted Array

--- 2-pointers/02-slow-and-fast/Practice.java
+++ 2-pointers/02-slow-and-fast/Practice.java
@@ -1,20 +1,17 @@
 class Practice {
 
   // 26. Remove Duplicates from Sorted Array
-  public int removeDuplicates(int[] nums) 
-  {
-    int output = 0; 
+  public int removeDuplicates(int[] nums) {
     int n = nums.length;
-
-    int slow = 0;
-    for (int fast = 1; fast < nums.length; fast++) 
-      if (nums[fast] != nums[slow]) {
-          slow++;
-          nums[slow] = nums[fast];
-      }
     
-    output = slow + 1;
-    return output; 
+    int slow = 0;
+    for (int fast = 1; fast < n; fast++) 
+    if (nums[fast] != nums[slow]) {
+        slow++;
+        nums[slow] = nums[fast];
+    }
+    
+    return slow + 1;
   }
 
 }
