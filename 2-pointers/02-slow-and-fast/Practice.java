885dfd5a4eebd4c627f72a056e154b9da6a4506e
Gregory Ross <gregross.dev@gmail.com>
2023-04-18 12:47:40 -0400

    189. Rotate Array

--- 2-pointers/02-slow-and-fast/Practice.java
+++ 2-pointers/02-slow-and-fast/Practice.java
@@ -14,4 +14,31 @@ class Practice {
     return slow + 1;
   }
 
+  // 189. Rotate Array
+  class RotateArray {
+    public void rotate(int[] nums, int k) {
+      int n = nums.length; 
+      k %= n; 
+
+      // rotate entire array 
+      reverse(nums, 0, n-1); 
+      // rotate k number at start of array
+      reverse(nums, 0, k-1); 
+      // rotate k number at end of array
+      reverse(nums, k, n-1); 
+    }
+        
+    public void reverse(int[] nums, int p1slow, int p2fast) {
+      while (p1slow < p2fast) {
+          int swap = nums[p1slow];
+          nums[p1slow] = nums[p2fast];
+          nums[p2fast] = swap;
+            
+          p1slow++;
+          p2fast--;  
+      }
+    }
+    
+}
+
 }
