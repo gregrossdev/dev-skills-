bfda1840abe217aa98615468a622ccf1bdb793e0
Gregory Ross <gregross.dev@gmail.com>
2023-04-19 09:43:51 -0400

    242. Valid Anagram

--- arrays-&-hashing/Practice.java
+++ arrays-&-hashing/Practice.java
@@ -11,6 +11,23 @@ class Practice {
 			
 			return false; 
 	}
+
+	// Easy 
+  // 242. Valid Anagram
+	public static boolean isAnagram(String s, String t) {
+		if (s.length() != t.length()) return false;
+
+		int[] freq = new int[26];
+		for (int p1 = 0; p1 < s.length(); p1++) {
+				freq[s.charAt(p1) - 'a']++;
+				freq[t.charAt(p1) - 'a']--;
+		}
+
+		for (int j: freq) 
+				if (j != 0) return false;
+		
+		return true;
+	}
   
 
 
