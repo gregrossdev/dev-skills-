e1f2b5ce582ce686a4ec501795e96fe9be97cf5c
Gregory Ross <gregross.dev@gmail.com>
2023-04-19 11:34:04 -0400

    704. Binary Search

---
+++ 2-pointers/03-merge-2-arrays/binary-search/Practice.java
@@ -0,0 +1,32 @@
+class Practice {
+
+    // Easy
+    // 704. Binary Search
+    public int search(int[] nums, int target) {
+			int output = -1; 
+			
+      int lp = 0;
+			int rp = nums.length - 1;
+			while (lp <= rp) {
+					int midpoint = (lp + rp) / 2;
+
+					if (nums[midpoint] == target) 
+						return output = midpoint; 
+					else if (nums[midpoint] < target) 
+						lp = midpoint + 1; 
+					else 
+						rp = midpoint - 1;
+			}
+
+        return output;
+    }
+}
+
+
+
+
+
+
+
+
+}
