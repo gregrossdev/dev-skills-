fd0feca200c55f753fa39a316cc119b028a83a75
Gregory Ross <gregross.dev@gmail.com>
2023-04-19 09:48:29 -0400

    1. Two Sum

--- arrays-&-hashing/Practice.java
+++ arrays-&-hashing/Practice.java
@@ -28,7 +28,26 @@ class Practice {
 		
 		return true;
 	}
-  
+
+	// Easy 
+  // 1. Two Sum
+  public int[] twoSum(int[] nums, int target) {
+		int[] output = new int[2]; 
+
+		Map<Integer, Integer> map = new HashMap<>(); 
+
+		for (int i = 0; i < nums.length; i++) 
+		{
+				if(map.containsKey(nums[i])) {
+						output[0] = map.get(nums[i]);
+						output[1] = i;
+						return output;
+				}
+				map.put(target - nums[i], i); 
+		}
+
+		return output; 
+	}
 
 
 }
