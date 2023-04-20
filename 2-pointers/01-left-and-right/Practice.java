778fd7fbeb9f2e1b0b4534423e6bd94e50299c1f
Gregory Ross <gregross.dev@gmail.com>
2023-04-20 12:25:02 -0400

    18. 4Sum

--- 2-pointers/01-left-and-right/Practice.java
+++ 2-pointers/01-left-and-right/Practice.java
@@ -1,7 +1,21 @@
 class Practice {
 
 
-	//---2 Sum problem
+	//--- 2 Sum problem coding pattern
+	// Easy
+	// 1. Two Sum	
+	public int[] twoSum(int[] nums, int target) {
+		int[] output = new int[2]; 
+		int n = nums.length;
+
+		for (int lp = 0; lp < n; lp++) 
+			for (int rp = lp + 1; rp < n; rp++) 
+				if(nums[lp] + nums[rp] == target)
+					output = new int[]{lp, rp}; 
+
+		return output; 
+	}
+
 	// Medium
 	// 167. Two Sum II - Input Array Is Sorted
 	public int[] twoSum(int[] numbers, int target) {
@@ -15,6 +29,83 @@ class Practice {
 		}
 		return output;
   }
+
+	// Medium
+  // 15. 3Sum
+  public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> output = new ArrayList<>();  
+		Arrays.sort(nums);
+
+		int n = nums.length; 
+		for (int i = 0; i < n; i++) {
+			if(i != 0 && nums[i]==nums[i-1]) continue; 
+
+			int temp = nums[i]; 
+			int lp = i + 1; 
+			int rp = n - 1; 
+		
+			while(lp < rp) {
+				int sum = temp + nums[lp] + nums[rp]; 
+				if(sum > 0) rp--; 
+				else if(sum < 0) lp++; 
+				else {
+					output.add(Arrays.asList(temp,nums[lp],nums[rp])); 
+					rp--; 
+					lp++;
+					while(nums[lp]==nums[lp-1] && lp < rp) 
+						lp++; 
+				}
+			}
+		}
+	
+		return output; 
+  }
+
+	// Medium
+  // 18. 4Sum
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+		List<List<Integer>> output = new ArrayList<>();  
+		int n = nums.length;
+		if (nums == null || n < 4) return output;
+		Arrays.sort(nums);
+
+		for (int lp1 = 0; lp1 < n - 3; lp1++) {
+			// Check for duplicates
+			if (lp1 > 0 && nums[lp1] == nums[lp1 - 1]) continue;
+			// Reduce to 3Sum problem
+			for (int lp2 = lp1 + 1; lp2 < n - 2; lp2++) {
+					// Check for duplicates
+					if(lp2 > lp1 + 1 && nums[lp2] == nums[lp2 -1]) continue; 
+					
+
+					int lp3 = lp2 + 1;
+					int rp = n - 1;
+					// Reduce to 2sum problem
+					while (lp3 < rp) {
+							int sum = nums[lp1] + nums[lp2] + nums[lp3] + nums[rp];
+							if(sum == target) {
+								List<Integer> result = new ArrayList<>(); 
+								result.add(nums[lp1]);
+								result.add(nums[lp2]); 
+								result.add(nums[lp3]); 
+								result.add(nums[rp]);
+								output.add(result); 
+								lp3++;
+								rp--; 
+								while (lp3 < rp && nums[lp3] == nums[lp3-1]) lp3++;  
+								while (lp3 < rp && nums[rp] == nums[rp+1]) rp--;  
+							}
+							else if(sum < target) 
+								lp3++;
+							else 
+								rp--; 
+					}
+				}
+			}
+		return output; 
+  }
+
+
 
 	// Easy
 	// 125. Valid Palindrome
@@ -88,37 +179,6 @@ class Practice {
 	}
 
 	return output; 
-  }
-
-  // Medium
-  // 15. 3Sum
-  public List<List<Integer>> threeSum(int[] nums) {
-	  List<List<Integer>> output = new ArrayList<>();  
-	  Arrays.sort(nums);
-
-	  int n = nums.length; 
-	  for (int i = 0; i < n; i++) {
-		if(i != 0 && nums[i]==nums[i-1]) continue; 
-
-		int temp = nums[i]; 
-		int lp = i + 1; 
-		int rp = n - 1; 
-		
-		while(lp < rp) {
-			int sum = temp + nums[lp] + nums[rp]; 
-			if(sum > 0) rp--; 
-			else if(sum < 0) lp++; 
-			else {
-				output.add(Arrays.asList(temp,nums[lp],nums[rp])); 
-				rp--; 
-				lp++;
-				while(nums[lp]==nums[lp-1] && lp < rp) 
-					lp++; 
-			}
-		}
-	  }
-	  
-	  return output; 
   }
 
   // 75. Sort Colors
