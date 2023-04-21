a140af601e9f992e5f490bf6e9d2a843f38919b6
Gregory Ross <gregross.dev@gmail.com>
2023-04-21 14:03:13 -0400

    8kyu Count of positives / sum of negatives

--- codewars/arrays/Practice.ts
+++ codewars/arrays/Practice.ts
@@ -1,7 +1,20 @@
-// 8ktu Sum of positive
+// 8kyu Sum of positive
 export function positiveSum(arr:number[]):number 
 {
   let output: number = 0; 
   arr.forEach(num => num > 0 && (output += num))
   return output; 
+}
+
+// 8kyu Count of positives / sum of negatives
+export function countPositivesSumNegatives(input: any): any[] | number[]
+{
+  if (input === null || input.length ===0) return [];
+
+  let output: number[] = [0, 0];
+  for (const num of input) {
+    output[0] += Math.max(0, Math.sign(num));
+    output[1] += Math.min(0, num);
+  }
+  return output;
 }
