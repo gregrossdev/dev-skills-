d3be17734e112846b475c8382d3c7ee535380309
Gregory Ross <gregross.dev@gmail.com>
2023-04-24 14:16:42 -0400

    30. Substring with Concatenation of All Words

--- leetcode/sliding-window/SlidingWindow.java
+++ leetcode/sliding-window/SlidingWindow.java
@@ -22,4 +22,45 @@ public class SlidingWindow {
     // O(n)
     return output;
   }
+
+  // Hard
+  // 30. Substring with Concatenation of All Words
+  public List<Integer> findSubstring(String s, String[] words) {
+    List<Integer> output = new ArrayList<>();
+    Map<String, Integer> keyValue = new HashMap<>();
+
+    if (s == "" || words.length == 0) return output;
+    
+    for (String word: words) 
+        keyValue.put(word, keyValue.getOrDefault(word, 0) + 1);
+
+
+    int arrSize = words.length;
+    int wordLength = words[0].length();
+    
+    for (int lp=0; lp<=s.length() - arrSize * wordLength; lp++) {
+        Map<String, Integer> wordsSeen = new HashMap<>();
+        
+        for (int rp=0; rp<arrSize; rp++) {
+            int nextWordIndex = lp + rp * wordLength;
+            String window = s.substring(nextWordIndex, nextWordIndex + wordLength);
+            
+            if (!keyValue.containsKey(window)) 
+              break;
+
+            wordsSeen.put(window, wordsSeen.getOrDefault(window, 0) + 1);
+            
+            if (wordsSeen.get(window) > keyValue.getOrDefault(window, 0)) 
+              break;
+
+            if (rp + 1 == arrSize) 
+              output.add(lp);
+        }
+    }
+    
+    return output;
+    
+  }
+
+
 }
