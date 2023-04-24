f368918b9bc1695313b6224de581ffa7004560ee
Gregory Ross <gregross.dev@gmail.com>
2023-04-24 10:49:59 -0400

    refactor

--- hackerrank/algorithms/warmup/easy/CompareTheTriplets.java
+++ hackerrank/algorithms/warmup/easy/CompareTheTriplets.java
@@ -1,18 +1,12 @@
 package easy;
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class CompareTheTriplets {
+class Result {
 
     /*
      * Complete the 'compareTriplets' function below.
@@ -40,7 +34,7 @@ class CompareTheTriplets {
 
 }
 
-public class Solution {
+public class CompareTheTriplets {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
