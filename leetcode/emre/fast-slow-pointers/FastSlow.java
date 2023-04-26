ed1b8f292ed34e588f584cecc89b010b1fb63f87
Gregory Ross <gregross.dev@gmail.com>
2023-04-26 09:50:32 -0400

    876. Middle of the Linked List

--- leetcode/emre/fast-slow-pointers/FastSlow.java
+++ leetcode/emre/fast-slow-pointers/FastSlow.java
@@ -87,6 +87,30 @@ class FastSlow {
       return n;
     }
   }
+
+
+  
+  /**
+   **876. Middle of the Linked List 
+   * Definition for singly-linked list.
+   * public class ListNode {
+   *     int val;
+   *     ListNode next;
+   *     ListNode() {}
+   *     ListNode(int val) { this.val = val; }
+   *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   * }
+   */
+  public ListNode middleNode(ListNode head) {
+      ListNode slow = head;
+      ListNode fast = head;
+
+      while(fast != null && fast.next != null) {
+          slow = slow.next;
+          fast = fast.next.next;
+      }
+      return slow;
+  }
   
 
 
