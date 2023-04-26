ea0044ff58ee5143fcc1af4ecac152ebfa3db149
Gregory Ross <gregross.dev@gmail.com>
2023-04-26 11:14:17 -0400

    binaryArrayToNumber

--- codewars/arrays/7kyu/Practice.ts
+++ codewars/arrays/7kyu/Practice.ts
@@ -2,3 +2,10 @@
 export function rowSumOddNumbers(n: number): number {
   return n ** 3; 
 }
+
+// Ones and Zeros
+export function binaryArrayToNumber(arr: number[]): number{
+  // First, we join the array into a string of "0"s and "1"s
+  // and then we parse it as a binary number using parseInt
+  return parseInt(arr.join(''), 2);
+};
