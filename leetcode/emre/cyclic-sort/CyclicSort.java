e57929a41556e6f54de9f55da2e6cd3a2b511171
Gregory Ross <gregross.dev@gmail.com>
2023-04-28 12:06:00 -0400

    442 - Find All Duplicates in an Array

--- leetcode/emre/cyclic-sort/CyclicSort.java
+++ leetcode/emre/cyclic-sort/CyclicSort.java
@@ -6,29 +6,59 @@
  */
 
 class CyclicSort {
-
   // LeetCode 268 - Missing Number [easy]
-  public int missingNumber(int[] nums) {
-    int n = nums.length;
+  class MissingNumber {
+    public int missingNumber(int[] nums) {
+      int n = nums.length;
 
-    int start = 0;
-    while (start < n)
-      if (nums[start] < n && start != nums[start])
-        swap(nums, start, nums[start]);
-      else
-        start++;
+      int start = 0;
+      while (start < n)
+        if (nums[start] < n && start != nums[start])
+          swap(nums, start, nums[start]);
+        else
+          start++;
 
-    for (int i = 0; i < n; i++)
-      if (nums[i] != i)
-        return i;
-    
+      for (int i = 0; i < n; i++)
+        if (nums[i] != i)
+          return i;
 
-    return n;
+      return n;
+    }
+
+    public void swap(int[] arr, int lp, int rp) {
+      int temp = arr[lp];
+      arr[lp] = arr[rp];
+      arr[rp] = temp;
+    }
   }
-  
-  public void swap(int[] arr, int first, int second){
-      int temp = arr[first];
-      arr[first] = arr[second];
-      arr[second] = temp;
+
+  // LeetCode 442 - Find All Duplicates in an Array [medium]
+  class FindDuplicates {
+    public List<Integer> findDuplicates(int[] nums) {
+      List<Integer> output = new ArrayList<Integer>();
+      int n = nums.length;
+
+      int start = 0;
+      while (start < n) {
+        int correct = nums[start] - 1;
+        if (nums[start] != nums[correct])
+          swap(nums, start, correct);
+        else
+          start++;
+      }
+
+      for (int i = 0; i < n; i++)
+        if (nums[i] != i + 1)
+          output.add(nums[i]);
+
+      return output;
+    }
+
+    public void swap(int[] arr, int lp, int rp) {
+      int temp = arr[lp];
+      arr[lp] = arr[rp];
+      arr[rp] = temp;
+    }
   }
+
 }
