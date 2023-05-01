de05d2781466a4ec4ab954b7b74ec205f93af445
Gregory Ross <gregross.dev@gmail.com>
2023-05-01 18:41:58 -0400

    Create Phone Number

--- codewars/arrays/6kyu/Practice.ts
+++ codewars/arrays/6kyu/Practice.ts
@@ -2,3 +2,9 @@
 export function arrayDiff(a: number[], b: number[]): number[] {
   return a.filter((num) => !b.includes(num))
 }
+
+// Create Phone Number
+export function createPhoneNumber(numbers: number[]): string {
+  let phone: string = numbers.join(''); 
+  return `(${phone.substring(0, 3)}) ${phone.substring(3, 6)}-${phone.substring(6)}`;
+}
