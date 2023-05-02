faed46bdc4e50003610392551400f807b64b4f3e
Gregory Ross <gregross.dev@gmail.com>
2023-05-02 08:25:42 -0400

    Duplicate Encoder

--- codewars/arrays/6kyu/Practice.ts
+++ codewars/arrays/6kyu/Practice.ts
@@ -8,3 +8,17 @@ export function createPhoneNumber(numbers: number[]): string {
   let phone: string = numbers.join(''); 
   return `(${phone.substring(0, 3)}) ${phone.substring(3, 6)}-${phone.substring(6)}`;
 }
+
+// Duplicate Encoder
+export function duplicateEncode(word: string){
+  let output: string = ""; 
+  word = word.toLowerCase(); 
+  for(let i=0; i<word.length; i++) {
+    if(word.lastIndexOf(word[i]) == word.indexOf(word[i]))
+      output += '('; 
+    else 
+      output += ')'; 
+  }
+
+  return output; 
+}
