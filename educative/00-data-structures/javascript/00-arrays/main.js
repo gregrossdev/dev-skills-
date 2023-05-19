be6313b98a8e8ef03069b1cf4a703f4618bcdd69
Gregory Ross <gregross.dev@gmail.com>
2023-05-19 10:50:51 -0400

    Challenge: Find Two Numbers that Add up to "value"

--- educative/00-data-structures/javascript/00-arrays/main.js
+++ educative/00-data-structures/javascript/00-arrays/main.js
@@ -65,3 +65,93 @@ function mergeArrays(arr1, arr2) {
 // time: O(n+m)
 console.log(mergeArrays([4,5,6],[-2,-1,0,7]))
 /////////////////////////////////////////////////////////////////////////////////
+//--Challenge: Find Two Numbers that Add up to "value"
+// Solution #1: Brute Force
+function findSum(arr, value) {
+  let n = arr.length; 
+  for (let j = 0; j < n; j++) 
+     for (let k = j + 1; k < n; k++) 
+          if ((arr[j] + arr[k]) == value)
+              return [arr[j], arr[k]]
+  return false;
+}
+
+// time: O(n^2)
+console.log(findSum([1, 2, 3, 4], 6))
+console.log(findSum([1, 2, 3, 4], 10))
+
+// Solution #2: Sorting the Array
+function binarySearch(arr, item){
+ let first = 0, mid;
+ let last = arr.length - 1;
+ let found = false
+ let arrayIndex = -1
+ while( (first <= last) && !found){
+     mid = Math.floor((first + last) / 2)
+     if( arr[mid] == item){
+         arrayIndex = mid
+         found = true
+     }
+     else{
+         if(item < arr[mid])
+             last = mid - 1
+         else
+             first = mid + 1
+     }
+ }
+ if (found)
+      return arrayIndex
+  else
+      return false
+}
+
+function findSum(arr,value){
+ arr.sort(function(a, b) {
+      return a - b
+  })
+var index;
+for(var j = 0; j<arr.length; j++){
+ index = binarySearch(arr,(value-arr[j]))
+ if(index!=false && j!=index)
+   return [arr[j],value-arr[j]]
+}
+return false;
+}
+
+// O(nlogn)
+console.log(findSum([1,2,3,4],5))
+console.log(findSum([4,3,2,1],9))
+
+// Solution #3: Moving indices
+function findSum(arr, value) {
+  arr.sort(function(a, b) {
+      return a - b
+  })
+  let index1 = 0,
+      index2 = arr.length - 1,
+      result = [],
+      sum = 0;
+
+  while (index1 != index2) {
+      sum = arr[index1] + arr[index2]
+
+      if (sum < value) {
+          index1++;
+      } 
+      else if (sum > value) {
+          index2--;
+      } 
+      else {
+          result.push(arr[index1]);
+          result.push(arr[index2]);
+          return result;
+      }
+  }
+
+return false;
+}
+
+// time: O(nlogn)
+console.log(findSum([1,2,3,4],5))
+console.log(findSum([1,2,3,4],10))
+/////////////////////////////////////////////////////////////////////////////////
