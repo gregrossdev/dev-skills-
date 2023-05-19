f3a296559c07f5c280ac4fbbc44a52363af7a03e
Gregory Ross <gregross.dev@gmail.com>
2023-05-19 18:05:29 -0400

    Challenge: Find Second Maximum Value in an Array

--- educative/00-data-structures/javascript/00-arrays/main.js
+++ educative/00-data-structures/javascript/00-arrays/main.js
@@ -1,12 +1,12 @@
 //--Challenge: Remove Even Integers From an Array
 // Solution 1: Doing it “by hand”
 function removeEven(arr) {
-  let odds = [];
-  for(let num of arr)
-      if(arr[num] % 2 != 0)
-          odds.push(arr[num])
+    let odds = [];
+    for (let num of arr)
+        if (arr[num] % 2 != 0)
+            odds.push(arr[num])
 
-  return odds; 
+    return odds;
 }
 
 // Solution #2: Using filter() and lambda function
@@ -14,11 +14,11 @@ function removeEven(arr) {
     return arr.filter((num => (num % 2) != 0))
 }
 // time: O(n)
-console.log(removeEven([3,2,41,3,34]))
+console.log(removeEven([3, 2, 41, 3, 34]))
 /////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Merge Two Sorted Arrays
 function mergeArrays(arr1, arr2) {
-		let merged = arr1.concat(arr2); 
+    let merged = arr1.concat(arr2);
     return merged.sort((a, b) => a - b);
 }
 
@@ -28,23 +28,23 @@ function mergeArrays(arr1, arr2) {
 }
 
 // time: O(nlogn)
-console.log(mergeArrays([4,5,6],[-2,-1,0,7]))
+console.log(mergeArrays([4, 5, 6], [-2, -1, 0, 7]))
 
 // Solution #2: Creating a new array
 function mergeArrays(arr1, arr2) {
     let merged = [];
     let i = 0,
         j = 0;
-  //Traverse both arrays and insert smaller value from arr1 or arr2
-  //into result array and then increment that array index.
- //If an array is completely traversed, while other one is left then just
- //copy all the remaining elements into result array
+    //Traverse both arrays and insert smaller value from arr1 or arr2
+    //into result array and then increment that array index.
+    //If an array is completely traversed, while other one is left then just
+    //copy all the remaining elements into result array
     while ((i < arr1.length) && (j < arr2.length)) {
         if (arr1[i] < arr2[j]) {
             merged.push(arr1[i]);
             i++;
-        } 
-				else {
+        }
+        else {
             merged.push(arr2[j]);
             j++;
         }
@@ -53,8 +53,8 @@ function mergeArrays(arr1, arr2) {
     if (i <= (arr1.length - 1)) {
         arr1.splice(0, i)
         merged = merged.concat(arr1);
-    } 
-		else if (j <= (arr2.length - 1)) {
+    }
+    else if (j <= (arr2.length - 1)) {
         arr2.splice(0, j)
         merged = merged.concat(arr2)
     }
@@ -63,17 +63,17 @@ function mergeArrays(arr1, arr2) {
 }
 
 // time: O(n+m)
-console.log(mergeArrays([4,5,6],[-2,-1,0,7]))
+console.log(mergeArrays([4, 5, 6], [-2, -1, 0, 7]))
 /////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Find Two Numbers that Add up to "value"
 // Solution #1: Brute Force
 function findSum(arr, value) {
-  let n = arr.length; 
-  for (let j = 0; j < n; j++) 
-     for (let k = j + 1; k < n; k++) 
-          if ((arr[j] + arr[k]) == value)
-              return [arr[j], arr[k]]
-  return false;
+    let n = arr.length;
+    for (let j = 0; j < n; j++)
+        for (let k = j + 1; k < n; k++)
+            if ((arr[j] + arr[k]) == value)
+                return [arr[j], arr[k]]
+    return false;
 }
 
 // time: O(n^2)
@@ -81,111 +81,111 @@ console.log(findSum([1, 2, 3, 4], 6))
 console.log(findSum([1, 2, 3, 4], 10))
 
 // Solution #2: Sorting the Array
-function binarySearch(arr, item){
- let first = 0, mid;
- let last = arr.length - 1;
- let found = false
- let arrayIndex = -1
- while( (first <= last) && !found){
-     mid = Math.floor((first + last) / 2)
-     if( arr[mid] == item){
-         arrayIndex = mid
-         found = true
-     }
-     else{
-         if(item < arr[mid])
-             last = mid - 1
-         else
-             first = mid + 1
-     }
- }
- if (found)
-      return arrayIndex
-  else
-      return false
+function binarySearch(arr, item) {
+    let first = 0, mid;
+    let last = arr.length - 1;
+    let found = false
+    let arrayIndex = -1
+    while ((first <= last) && !found) {
+        mid = Math.floor((first + last) / 2)
+        if (arr[mid] == item) {
+            arrayIndex = mid
+            found = true
+        }
+        else {
+            if (item < arr[mid])
+                last = mid - 1
+            else
+                first = mid + 1
+        }
+    }
+    if (found)
+        return arrayIndex
+    else
+        return false
 }
 
-function findSum(arr,value){
- arr.sort(function(a, b) {
-      return a - b
-  })
-var index;
-for(var j = 0; j<arr.length; j++){
- index = binarySearch(arr,(value-arr[j]))
- if(index!=false && j!=index)
-   return [arr[j],value-arr[j]]
-}
-return false;
+function findSum(arr, value) {
+    arr.sort(function (a, b) {
+        return a - b
+    })
+    var index;
+    for (var j = 0; j < arr.length; j++) {
+        index = binarySearch(arr, (value - arr[j]))
+        if (index != false && j != index)
+            return [arr[j], value - arr[j]]
+    }
+    return false;
 }
 
 // O(nlogn)
-console.log(findSum([1,2,3,4],5))
-console.log(findSum([4,3,2,1],9))
+console.log(findSum([1, 2, 3, 4], 5))
+console.log(findSum([4, 3, 2, 1], 9))
 
 // Solution #3: Moving indices
 function findSum(arr, value) {
-  arr.sort(function(a, b) {
-      return a - b
-  })
-  let index1 = 0,
-      index2 = arr.length - 1,
-      result = [],
-      sum = 0;
+    arr.sort(function (a, b) {
+        return a - b
+    })
+    let index1 = 0,
+        index2 = arr.length - 1,
+        result = [],
+        sum = 0;
 
-  while (index1 != index2) {
-      sum = arr[index1] + arr[index2]
+    while (index1 != index2) {
+        sum = arr[index1] + arr[index2]
 
-      if (sum < value) {
-          index1++;
-      } 
-      else if (sum > value) {
-          index2--;
-      } 
-      else {
-          result.push(arr[index1]);
-          result.push(arr[index2]);
-          return result;
-      }
-  }
+        if (sum < value) {
+            index1++;
+        }
+        else if (sum > value) {
+            index2--;
+        }
+        else {
+            result.push(arr[index1]);
+            result.push(arr[index2]);
+            return result;
+        }
+    }
 
-return false;
+    return false;
 }
 
 // time: O(nlogn)
-console.log(findSum([1,2,3,4],5))
-console.log(findSum([1,2,3,4],10))
+console.log(findSum([1, 2, 3, 4], 5))
+console.log(findSum([1, 2, 3, 4], 10))
 /////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Array of Products of All Elements
 // Solution #1: Using a nested loop
-function findProduct(arr){
-  var result = []
-  var left = 1, currentproduct;
-  //To store product of all previous values from currentIndex
-  for(var i=0;i<arr.length;i++){
-    currentproduct = 1
-    //To store current product for index i    
-		//compute product of values to the right of i index of list
-    for(var j=i+1;j<arr.length;j++){
-      currentproduct = currentproduct * arr[j]
+function findProduct(arr) {
+    var result = []
+    var left = 1, currentproduct;
+    //To store product of all previous values from currentIndex
+    for (var i = 0; i < arr.length; i++) {
+        currentproduct = 1
+        //To store current product for index i    
+        //compute product of values to the right of i index of list
+        for (var j = i + 1; j < arr.length; j++) {
+            currentproduct = currentproduct * arr[j]
+        }
+        //currentproduct * product of all values to the left of i index
+        result.push(currentproduct * left)
+        //Updating `left`
+        left = left * arr[i]
     }
-    //currentproduct * product of all values to the left of i index
-    result.push(currentproduct * left)
-    //Updating `left`
-    left = left * arr[i]
-  }
-    
-  return result
+
+    return result
 }
 
 // time: O(n^2)
-console.log(findProduct([1,2,3,4,5]))
+console.log(findProduct([1, 2, 3, 4, 5]))
 
 // Solution #2: Optimizing the number of multiplications
 function findProduct(arr) {
     var temp = 1,
         product = [];
-    for (var i = 0; i < arr.length ; i++) {
-        product[i]  = temp;
+    for (var i = 0; i < arr.length; i++) {
+        product[i] = temp;
         temp = temp * arr[i];
     }
 
@@ -199,126 +199,172 @@ function findProduct(arr) {
 }
 
 // time: O(n)
-console.log(findProduct([1,3,4,5]))
+console.log(findProduct([1, 3, 4, 5]))
 /////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Find Minimum Value in Array
 // Solution #1: Using a nested loop
-function findProduct(arr){
+function findProduct(arr) {
     var result = []
     var left = 1, currentproduct;
     //To store product of all previous values from currentIndex
-    for(var i=0;i<arr.length;i++){
-      currentproduct = 1
-      //To store current product for index i    
-          //compute product of values to the right of i index of list
-      for(var j=i+1;j<arr.length;j++){
-        currentproduct = currentproduct * arr[j]
-      }
-      //currentproduct * product of all values to the left of i index
-      result.push(currentproduct * left)
-      //Updating `left`
-      left = left * arr[i]
+    for (var i = 0; i < arr.length; i++) {
+        currentproduct = 1
+        //To store current product for index i    
+        //compute product of values to the right of i index of list
+        for (var j = i + 1; j < arr.length; j++) {
+            currentproduct = currentproduct * arr[j]
+        }
+        //currentproduct * product of all values to the left of i index
+        result.push(currentproduct * left)
+        //Updating `left`
+        left = left * arr[i]
     }
-      
+
     return result
-  }
-  
-  // time: O(n^2)
-  console.log(findProduct([1,2,3,4,5]))
-  
-  // Solution #2: Optimizing the number of multiplications
-  function findProduct(arr) {
-      var temp = 1,
-          product = [];
-      for (var i = 0; i < arr.length ; i++) {
-          product[i]  = temp;
-          temp = temp * arr[i];
-      }
-  
-      temp = 1;
-      for (var i = arr.length - 1; i > -1; i--) {
-          product[i] *= temp;
-          temp *= arr[i];
-      }
-  
-      return product
-  }
-  
-  // time: O(n)
-  console.log(findProduct([1,3,4,5]))
-  /////////////////////////////////////////////////////////////////////////////////
+}
+
+// time: O(n^2)
+console.log(findProduct([1, 2, 3, 4, 5]))
+
+// Solution #2: Optimizing the number of multiplications
+function findProduct(arr) {
+    var temp = 1,
+        product = [];
+    for (var i = 0; i < arr.length; i++) {
+        product[i] = temp;
+        temp = temp * arr[i];
+    }
+
+    temp = 1;
+    for (var i = arr.length - 1; i > -1; i--) {
+        product[i] *= temp;
+        temp *= arr[i];
+    }
+
+    return product
+}
+
+// time: O(n)
+console.log(findProduct([1, 3, 4, 5]))
+/////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Find Minimum Value in Array
 // Solution #1: Sort the array
-function findMinimum(arr){
-    arr.sort((function(a, b){
-          return a - b
-      }))
+function findMinimum(arr) {
+    arr.sort((function (a, b) {
+        return a - b
+    }))
     return arr[0]
-  }
-  
-  //--time: O(nlogn)
-  console.log(findMinimum([9,2,3,6,-1]))
-  
-  // Solution #2: Iterate over the array#
-  function findMinimum(arr) {
-      var currentMin = arr[0];
-      //At every Index compare its value with current minimum 
-      //and if its less, then make that index value the new minimum value
-      for (let val of arr) {
-          if (val < currentMin)
-              currentMin = val
-      }
-  
-      return currentMin
-  }
-  
-  //--time: O(n)
-  console.log(findMinimum([9,2,3,6,-1]))
-  /////////////////////////////////////////////////////////////////////////////////
+}
+
+//--time: O(nlogn)
+console.log(findMinimum([9, 2, 3, 6, -1]))
+
+// Solution #2: Iterate over the array#
+function findMinimum(arr) {
+    var currentMin = arr[0];
+    //At every Index compare its value with current minimum 
+    //and if its less, then make that index value the new minimum value
+    for (let val of arr) {
+        if (val < currentMin)
+            currentMin = val
+    }
+
+    return currentMin
+}
+
+//--time: O(n)
+console.log(findMinimum([9, 2, 3, 6, -1]))
+/////////////////////////////////////////////////////////////////////////////////
 //--Challenge: Find First Unique Integer in an Array
 // Solution #1
-function findFirstUnique(arr){
+function findFirstUnique(arr) {
     var index2;
-    for(var index1=0;index1<arr.length;index1++){ 
+    for (var index1 = 0; index1 < arr.length; index1++) {
         index2 = 0
-        while(index2 < arr.length){
-            if ((index1 != index2 )&& (arr[index1] == arr[index2])) 
+        while (index2 < arr.length) {
+            if ((index1 != index2) && (arr[index1] == arr[index2]))
                 break
             index2 += 1
         }
-        if (index2 == arr.length) 
-            return arr[index1] 
-        
+        if (index2 == arr.length)
+            return arr[index1]
+
     }
-      return null;
-  }
-    
-  // time: O(n^2)
-  console.log(findFirstUnique([ 9, 2, 3, 6,2, 6 ,9,0,3]))
+    return null;
+}
+
+// time: O(n^2)
+console.log(findFirstUnique([9, 2, 3, 6, 2, 6, 9, 0, 3]))
 
 /////////////////////////////////////////////////////////////////////////////////
-//--Challenge: 
-// Solution #1: 
+//--Challenge: Find Second Maximum Value in an Array
+// Solution #1: Sort and index
+function findSecondMaximum(arr) {
+    arr.sort(function (a, b) {
+        return a - b
+    })
+    if (arr.length >= 2)
+        return arr[arr.length - 2]
+    else
+        return null
+}
+
+// time: O(nlogn)
+console.log(findSecondMaximum([9, 2, 3, 6]))
+
+// Solution #2: Traversing the array twice
+function findSecondMaximum(arr) {
+    var firstMax = Number.NEGATIVE_INFINITY;
+    var secondmax = Number.NEGATIVE_INFINITY;
+    for (let item of arr) {
+        if (item > firstMax)
+            firstMax = item
+    }
+    for (let item of arr) {
+        if ((item < firstMax) && (item > secondmax))
+            secondmax = item
+    }
+    return secondmax
+}
+
+// time: O(n)
+console.log(findSecondMaximum([9, 2, 3, 6]))
+
+// Solution #3: Finding the Second Largest in one Traversal
+function findSecondMaximum(arr) {
+    var max = Number.NEGATIVE_INFINITY;
+    var secondmax = Number.NEGATIVE_INFINITY;
+    for (var val of arr) {
+        if (val > max) {
+            secondmax = max
+            max = val
+        } else if (val > secondmax && val != max)
+            secondmax = val
+    }
+    return secondmax
+}
+
+// time: O(n)
+console.log(findSecondMaximum([9, 2, 3, 6]))
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-//--Challenge: 
-// Solution #1: 
+//--Challenge:
+// Solution #1:
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-//--Challenge: 
-// Solution #1: 
+//--Challenge:
+// Solution #1:
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-//--Challenge: 
-// Solution #1: 
+//--Challenge:
+// Solution #1:
 
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-//--Challenge: 
-// Solution #1: 
+//--Challenge:
+// Solution #1:
 
 /////////////////////////////////////////////////////////////////////////////////
-  
