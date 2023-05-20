e352079bfc893ec686be80efad3900e94f39c6a2
Gregory Ross <gregross.dev@gmail.com>
2023-05-20 16:22:16 -0400

    Challenge: Array of Products of All Elements

--- educative/00-data-structures/javascript/00-arrays/Main.java
+++ educative/00-data-structures/javascript/00-arrays/Main.java
@@ -97,136 +97,186 @@ class Main {
   }
 
   // Solution #1: Brute Force
-class CheckSum{   
+  class CheckSum{   
 
-  public static int[] findSum(int[] arr, int n) 
-  {    
-    int[] result = new int[2]; // to store the pair of values.
-    
-    for (int i = 0; i < arr.length; i++) { //traverse arr
-      for (int j = i + 1; j < arr.length; j++) { //traverse arr again
-        if (arr[i] + arr[j] == n) { // find where sum is equal to n
-          result[0] = arr[i];
-          result[1] = arr[j];
-          return result; // return the pair of numbers
+    public static int[] findSum(int[] arr, int n) 
+    {    
+      int[] result = new int[2]; // to store the pair of values.
+      
+      for (int i = 0; i < arr.length; i++) { //traverse arr
+        for (int j = i + 1; j < arr.length; j++) { //traverse arr again
+          if (arr[i] + arr[j] == n) { // find where sum is equal to n
+            result[0] = arr[i];
+            result[1] = arr[j];
+            return result; // return the pair of numbers
+          }
         }
       }
+      return arr;  
     }
-    return arr;  
-  }
-	
-	// time: O(n^2)
-  public static void main(String args[]) {
+    
+    // time: O(n^2)
+    public static void main(String args[]) {
 
-    int n = 9;
-    int[] arr1 = {2, 4, 5, 7, 8};
-    if(arr1.length > 0){
-      int[] arr2 = findSum(arr1, n);
-      int num1 = arr2[0];
-      int num2 = arr2[1];
+      int n = 9;
+      int[] arr1 = {2, 4, 5, 7, 8};
+      if(arr1.length > 0){
+        int[] arr2 = findSum(arr1, n);
+        int num1 = arr2[0];
+        int num2 = arr2[1];
 
-      if((num1 + num2) != n)
-        System.out.println("Not Found");
-      else {
-        System.out.println("Number 1 = " + num1);
-        System.out.println("Number 2 = " + num2);
-        System.out.println("Sum = " +  (n) );
+        if((num1 + num2) != n)
+          System.out.println("Not Found");
+        else {
+          System.out.println("Number 1 = " + num1);
+          System.out.println("Number 2 = " + num2);
+          System.out.println("Sum = " +  (n) );
 
+        }
+      } else {
+        System.out.println("Input Array is Empty!");
       }
-    } else {
-      System.out.println("Input Array is Empty!");
     }
   }
-}
 
-// Solution #2: Sorting the Array
-class CheckSum{ 
+  // Solution #2: Sorting the Array
+  class CheckSum{ 
 
-  //Helper Function to sort given Array (Quick Sort)          
-  public static int partition(int[] arr, int low, int high) { 
-    int pivot = arr[high];  
-    int i = (low - 1); // index of smaller element 
-    for (int j = low; j < high; j++) { 
-      // If current element is <= to pivot 
-      if (arr[j] <= pivot) { 
-        i++; 
+    //Helper Function to sort given Array (Quick Sort)          
+    public static int partition(int[] arr, int low, int high) { 
+      int pivot = arr[high];  
+      int i = (low - 1); // index of smaller element 
+      for (int j = low; j < high; j++) { 
+        // If current element is <= to pivot 
+        if (arr[j] <= pivot) { 
+          i++; 
 
-        // swap arr[i] and arr[j] 
-        int temp = arr[i]; 
-        arr[i] = arr[j]; 
-        arr[j] = temp; 
+          // swap arr[i] and arr[j] 
+          int temp = arr[i]; 
+          arr[i] = arr[j]; 
+          arr[j] = temp; 
+        } 
+      } 
+
+      // swap arr[i+1] and arr[high] (or pivot) 
+      int temp = arr[i + 1]; 
+      arr[i + 1] = arr[high]; 
+      arr[high] = temp; 
+
+      return i + 1; 
+    } 
+
+    public static void sort(int[] arr, int low, int high) { 
+      if (low < high) { 
+        int pi = partition(arr, low, high); 
+        sort(arr, low, pi - 1); 
+        sort(arr, pi + 1, high); 
       } 
     } 
 
-    // swap arr[i+1] and arr[high] (or pivot) 
-    int temp = arr[i + 1]; 
-    arr[i + 1] = arr[high]; 
-    arr[high] = temp; 
+    //Return 2 elements of arr that sum to the given value
+    public static int[] findSum(int[] arr, int n) {
+      //Helper sort function that uses the Quicksort Algorithm
+      sort(arr, 0, arr.length - 1);   //Sort the array in Ascending Order
 
-    return i + 1; 
-  } 
+      int Pointer1 = 0;    //Pointer 1 -> At Start
+      int Pointer2 = arr.length - 1;   //Pointer 2 -> At End
 
-  public static void sort(int[] arr, int low, int high) { 
-    if (low < high) { 
-      int pi = partition(arr, low, high); 
-      sort(arr, low, pi - 1); 
-      sort(arr, pi + 1, high); 
+      int[] result = new int[2];
+      int sum = 0;
+
+      while (Pointer1 != Pointer2) {
+
+        sum = arr[Pointer1] + arr[Pointer2];  //Calulate Sum of Pointer 1 and 2
+
+        if (sum < n) 
+          Pointer1++;  //if sum is less than given value => Move Pointer 1 to Right
+        else if (sum > n) 
+          Pointer2--; 
+        else {
+          result[0] = arr[Pointer1];
+          result[1] = arr[Pointer2];
+          return result; // containing 2 number 
+        }
+      }
+      return arr; 
     } 
-  } 
+    
+    // time: O(nlogn)
+    public static void main(String args[]) {
 
-  //Return 2 elements of arr that sum to the given value
-  public static int[] findSum(int[] arr, int n) {
-    //Helper sort function that uses the Quicksort Algorithm
-    sort(arr, 0, arr.length - 1);   //Sort the array in Ascending Order
+      int n = 9;
+      int[] arr1 = {1,2,3,4,5};
+      if(arr1.length > 0){
+        int[] arr2 = findSum(arr1, n);
+        int num1 = arr2[0];
+        int num2 = arr2[1];
 
-    int Pointer1 = 0;    //Pointer 1 -> At Start
-    int Pointer2 = arr.length - 1;   //Pointer 2 -> At End
+        if((num1 + num2) != n)
+          System.out.println("Not Found");
+        else {
+          System.out.println("Number 1 = " + num1);
+          System.out.println("Number 2 = " + num2);
+          System.out.println("Sum = " +  (n) );
 
-    int[] result = new int[2];
-    int sum = 0;
-
-    while (Pointer1 != Pointer2) {
-
-      sum = arr[Pointer1] + arr[Pointer2];  //Calulate Sum of Pointer 1 and 2
-
-      if (sum < n) 
-        Pointer1++;  //if sum is less than given value => Move Pointer 1 to Right
-      else if (sum > n) 
-        Pointer2--; 
-      else {
-        result[0] = arr[Pointer1];
-        result[1] = arr[Pointer2];
-        return result; // containing 2 number 
+        }
+      } else {
+        System.out.println("Input Array is Empty!");
       }
-    }
-    return arr; 
-  } 
-	
-	// time: O(nlogn)
-  public static void main(String args[]) {
-
-    int n = 9;
-    int[] arr1 = {1,2,3,4,5};
-    if(arr1.length > 0){
-      int[] arr2 = findSum(arr1, n);
-      int num1 = arr2[0];
-      int num2 = arr2[1];
-
-      if((num1 + num2) != n)
-        System.out.println("Not Found");
-      else {
-        System.out.println("Number 1 = " + num1);
-        System.out.println("Number 2 = " + num2);
-        System.out.println("Sum = " +  (n) );
-
-      }
-    } else {
-      System.out.println("Input Array is Empty!");
     }
   }
-}
 
+  class ProductArray  {  
+    public static int[] findProduct(int arr[]) { 
+      int n = arr.length;
+      int i, temp = 1; 
   
+      // Allocation of result array
+      int result[] = new int[n]; 
+  
+      // Product of elements on left side excluding arr[i]
+      for (i = 0; i < n; i++)  
+      { 
+        result[i] = temp; 
+        temp *= arr[i]; 
+      } 
+  
+      // Initializing temp to 1 for product on right side
+      temp = 1; 
+  
+      // Product of elements on right side excluding arr[i] 
+      for (i = n - 1; i >= 0; i--)  
+      { 
+        result[i] *= temp; 
+        temp *= arr[i]; 
+      }
+  
+      return result; 
+    } 
+    public static String arrayToString(int arr[]){
+      if (arr.length > 0){
+        String result = "";
+        for(int i = 0; i < arr.length; i++) {
+          result += arr[i] + " ";
+        }
+        return result;
+      }
+      else {
+        return "Empty Array!";
+      }
+    }
+  
+    public static void main(String args[]) {
+  
+      int[] arr = {-1, 2, -3, 4, -5};
+  
+      System.out.println("Array before product: " + arrayToString(arr));
+  
+      int[] prodArray = findProduct(arr);
+  
+      System.out.println("Array after product: " + arrayToString(prodArray));
+    }
+  }
 
 
 
