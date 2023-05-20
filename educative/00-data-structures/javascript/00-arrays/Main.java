40c5d6b8d8368133074396249a3efb037a4d376d
Gregory Ross <gregross.dev@gmail.com>
2023-05-20 16:29:58 -0400

    Challenge: Find Second Maximum Value in an Array

--- educative/00-data-structures/javascript/00-arrays/Main.java
+++ educative/00-data-structures/javascript/00-arrays/Main.java
@@ -313,6 +313,110 @@ class Main {
 
 }
 
+// Solution #1: Traversing the Array Twice
+class CheckSecondMax {
 
+  public static int findSecondMaximum(int[] arr) {
+
+    int max = Integer.MIN_VALUE;;
+    int secondmax = Integer.MIN_VALUE;
+
+    // Find the maximum value in the array by comparing each index with max
+    // If an element is greater than max then update max to be equal to it
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) 
+        max = arr[i];
+
+    }//end of for-loop
+
+    // Find the second maximum value by comparing each index with secondmax
+    // If an element is greater than secondmax and not equal to previously 
+    // calculated max, then update secondmax to be equal to that element.
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > secondmax && arr[i] < max) 
+        secondmax = arr[i];
+
+    }//end of for-loop
+
+    return secondmax;
+  } 
+
+  public static String arrayToString(int arr[]){
+    if (arr.length > 0){
+      String result = "";
+      for(int i = 0; i < arr.length; i++) {
+        result += arr[i] + " ";
+      }
+      return result;
+    }
+    else {
+      return "Empty Array!";
+    }
+  }
+
+	// time: O(n)
+  public static void main(String args[]) {
+
+    int[] arr = {-2, -33, -10, -456};
+
+    System.out.println("Array: " + arrayToString(arr));
+
+    int secMax = findSecondMaximum(arr);
+
+    System.out.println("Second maximum: " + secMax);
+
+  }
+}
+
+// Solution #2: Traversing the Array Only Once#
+class CheckSecondMax {
+  //Returns second maximum value from given Array 
+  public static int findSecondMaximum(int[] arr) {
+
+    int max = Integer.MIN_VALUE;;
+    int secondmax = Integer.MIN_VALUE;
+
+    // Keep track of Maximum value, whenever the value at an array index is greater
+    // than current Maximum value then make that max value 2nd max value and
+    // make that index value maximum value  
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        secondmax = max;
+        max = arr[i];
+      }
+      else if (arr[i] > secondmax && arr[i] != max) {
+        secondmax = arr[i];
+      }
+    }//end of for-loop
+
+    return secondmax;
+  } //end of findSecondMaximum
+  
+  public static String arrayToString(int arr[]){
+    if (arr.length > 0){
+      String result = "";
+      for(int i = 0; i < arr.length; i++) {
+        result += arr[i] + " ";
+      }
+      return result;
+    }
+    else {
+      return "Empty Array!";
+    }
+  }
+
+	// time: O(n)
+  public static void main(String args[]) {
+
+    int[] arr = {-2, -33, -10, -456};
+
+    System.out.println("Array: " + arrayToString(arr));
+
+    int secMax = findSecondMaximum(arr);
+
+    System.out.println("Second maximum: " + secMax);
+
+  }
+}
 
 }
