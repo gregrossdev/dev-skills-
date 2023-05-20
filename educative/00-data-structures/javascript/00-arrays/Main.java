a4a731858485206831615828e4991674c8ff744e
Gregory Ross <gregross.dev@gmail.com>
2023-05-20 16:36:21 -0400

    Challenge: Rearrange Positive & Negative Values

--- educative/00-data-structures/javascript/00-arrays/Main.java
+++ educative/00-data-structures/javascript/00-arrays/Main.java
@@ -457,6 +457,86 @@ class CheckRotateArray {
 
 }
 
+// Solution# 1: Using New Array
+class CheckReArrange {
 
+  //Re-Arrange Positive and Negative Values of Given Array 
+  public static void reArrange(int arr[]) {
+
+    int[] newArray = new int[arr.length];
+    int newArrayIndex = 0;
+
+    //Fill newArray with Negative Values first.
+    //Then fill it with postive values.
+    //In the end, insert every element of newArray back into original arr.
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < 0)
+        newArray[newArrayIndex++] = arr[i];
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+
+      if (arr[i] >= 0)
+        newArray[newArrayIndex++] = arr[i];
+    }
+
+    for (int j = 0; j < newArrayIndex; j++) {
+      arr[j] = newArray[j];
+    }
+  }
+	
+	// time: O(n)
+  public static void main(String args[]) {
+    
+    int[] arr = {2, 4, -6, 8, -5, -10};
+
+    System.out.print("Array before re-arranging: ");
+    for(int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+
+    reArrange(arr);
+
+    System.out.print("Array after rearranging: ");
+    for(int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+  }
+}
+
+// Solution# 2: Re-arranging in Place
+class CheckReArrange {
+  //Re-Arrange Positive and Negative Values of Given Array 
+  public static void reArrange(int[] arr) 
+  {
+    int j = 0; 
+    for (int i = 0; i < arr.length; i++) { 
+      if (arr[i] < 0) {   // if negative number found
+        if (i != j) { 
+          int temp = arr[i];
+          arr[i] = arr[j]; // swapping with leftmost positive
+          arr[j] = temp;
+        }
+        j++; 
+      } 
+    } 
+  } //end of reArrange()
+
+	// time: O(n)
+  public static void main(String args[]) {
+
+    int[] arr = {2, 4, -6, 8, -5, -10};
+
+    System.out.print("Array before re-arranging: ");
+    for(int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+
+    reArrange(arr);
+
+    System.out.print("Array after rearranging: ");
+    for(int i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+  }
+}
 
 }
