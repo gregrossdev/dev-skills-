b4df435ab37d3b6587ccb909274711acd67c8e97
Greg Ross <gregross.dev@gmail.com>
2024-03-21 10:03:19 -0400

    Easy: i struggled to solve.

---
+++ src/hackerrank/algorithms/implementation/Q21E_DrawingBook.java
@@ -0,0 +1,9 @@
+package hackerrank.algorithms.implementation;
+
+public class Q21E_DrawingBook {
+    public static int pageCount(int n, int p) {
+        int fromStart = p / 2; // Number of pages turned from the start
+        int fromEnd = n / 2 - p / 2; // Number of pages turned from the end (Total pages from start - pages from start)
+        return Math.min(fromStart, fromEnd); // Return the minimum of pages turned
+    }
+}
