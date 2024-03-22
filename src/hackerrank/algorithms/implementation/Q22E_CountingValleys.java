f68071f37e3a5e2e6de230884e6de57f4efe4227
Greg Ross <gregross.dev@gmail.com>
2024-03-22 09:32:06 -0400

    Easy: string to charArray and flag for switching states to show status

---
+++ src/hackerrank/algorithms/implementation/Q22E_CountingValleys.java
@@ -0,0 +1,26 @@
+package hackerrank.algorithms.implementation;
+
+public class Q22E_CountingValleys {
+    public static int countingValleys(int steps, String path) {
+        // count the number of valleys walked through.
+        int count = 0;
+
+        int seaLevel = 0;
+        boolean inValley = false;
+
+        char[] charArray = path.toCharArray();
+        for(char letter: charArray) {
+            if(letter == 'U') seaLevel++;
+            else if(letter == 'D') seaLevel--;
+
+            if(seaLevel == 0) {
+                if(inValley) count++;
+                inValley = false;
+            }
+            else if(seaLevel < 0) inValley = true;
+        }
+
+
+        return count;
+    }
+}
