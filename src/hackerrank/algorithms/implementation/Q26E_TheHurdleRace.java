e0b083c7f7170815e1fa100f859b2ef9b14498ff
Greg Ross <gregross.dev@gmail.com>
2024-03-28 20:09:21 -0400

    Easy - simple to solve

---
+++ src/hackerrank/algorithms/implementation/Q26E_TheHurdleRace.java
@@ -0,0 +1,17 @@
+package hackerrank.algorithms.implementation;
+
+import java.util.List;
+
+public class Q26E_TheHurdleRace {
+    public static int hurdleRace(int k, List<Integer> height) {
+        // How many doses of the potion must the character take to be able to jump all of the hurdles.
+        // max number in list - max jump
+        int max = 0;
+        for (int maxHeight : height)
+            if(maxHeight > max) max = maxHeight;
+
+        int potions = max - k;
+        if(potions > 0) return potions;
+        else return 0;
+    }
+}
