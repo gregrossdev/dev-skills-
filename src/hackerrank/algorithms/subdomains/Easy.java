5f769a9cea8baed4ec6fd7f0f50010e00484c252
Greg Ross <gregross.dev@gmail.com>
2024-04-25 09:22:05 -0400

    Jumping on the Clouds: learning how to solve

--- src/hackerrank/algorithms/subdomains/Easy.java
+++ src/hackerrank/algorithms/subdomains/Easy.java
@@ -882,6 +882,25 @@ public class Easy {
     }
 
     // 42. Jumping on the Clouds
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Find the minimum number of jumps required to reach the last cloud
+        int jumps = 0;
+        int i = 0;
+
+        while (i < c.size() - 1) {
+            // Check if a jump of size 2 is possible
+            if (i + 2 < c.size() && c.get(i + 2) == 0) {
+                i += 2; // Jump 2 clouds
+            } else {
+                i++; // Jump 1 cloud
+            }
+
+            jumps++; // Increment the jump count
+        }
+
+        return jumps;
+
+    }
 
     // 43. Equalize the Array
 
