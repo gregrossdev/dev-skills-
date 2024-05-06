6b9525b21f16712749f8237bc9c9b14cd3a679b4
Greg Ross <gregross.dev@gmail.com>
2024-05-06 11:13:04 -0400

    Maximum Average Subarray I: I was unclear on the instructions. Eventually i had a better understanding of the Ask.

---
+++ src/leetcode/studyplan/leetcode_75/SlidingWindow.java
@@ -0,0 +1,34 @@
+package leetcode.studyplan.leetcode_75;
+
+public class SlidingWindow {
+	/*
+	 * Easy
+	 ******************/
+	// 643. Maximum Average Subarray I
+	// 643. Maximum Average Subarray I
+	public double findMaxAverage(int[] nums, int k) {
+		// Find a contiguous subarray whose length has a max average value
+		double maxAverage = 0;
+
+		// Calculate the sum of the first 'k' elements
+		double average = 0;
+		for (int idx = 0; idx < k; idx++) {
+			average += nums[idx];
+		}
+
+		// Calculate the average of the first 'k' elements
+		maxAverage = average / k;
+
+		// Slide the window through the rest of the array
+		for (int idx = k; idx < nums.length; idx++) {
+			// Add the new element to the window and remove the first element from the window
+			average += nums[idx] - nums[idx - k];
+			// Update maxAverage if the current average is greater
+			maxAverage = Math.max(maxAverage, average / k);
+		}
+
+		// Return the maximum average
+		return maxAverage;
+	}
+
+}
