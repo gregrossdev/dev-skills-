879c56d71fb9c772bb151c1963fac0358618310e
Greg Ross <gregross.dev@gmail.com>
2024-12-02 12:11:23 -0500

    Vowel Count

---
+++ src/test/java/dev/gregross/codewars/kyu7/VowelsTest.java
@@ -0,0 +1,38 @@
+package dev.gregross.codewars.kyu7;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VowelsTest {
+    private int solution(String str) {
+        return str.replaceAll("(?i)[^aiueo]", "").length();
+    }
+
+    private int random(int min, int max){
+        return (int) Math.floor(Math.random()*(max-min+1) + min);
+    }
+
+    @Test
+    public void testCase() {
+        assertEquals(5, Vowels.getCount("abracadabra"), "Input: 'abracadabra'\n\n");
+        assertEquals(0, Vowels.getCount(""), "Input: ''\n\n");
+        assertEquals(4, Vowels.getCount("pear tree"), "Input: 'pear tree'\n\n");
+        assertEquals(13, Vowels.getCount("o a kak ushakov lil vo kashu kakao"), "Input: 'o a kak ushakov lil vo kashu kakao'\n\n");
+        assertEquals(168, Vowels.getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), "Input: 'tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty'\n\n");
+    }
+
+    @Test
+    public void randomCase() {
+        String alpha = " abcdefghijklmnopqrstuvwxyz";
+
+        for(int i = 0; i++ < 100;){
+            StringBuilder str = new StringBuilder();
+            int x = random(20, 100);
+
+            for(int j = 0; j++ < x;)
+                str.append(alpha.charAt(random(0, 26)));
+
+            assertEquals(solution(str.toString()), Vowels.getCount(str.toString()), String.format("Input: '%s'\n\n", str.toString()));
+        }
+    }
+}
